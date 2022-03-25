cef0ba813dd3db62c2149a96be4bde9ed79fef2c
Cody McCarty <cmccarty@catalyte.io>
2022-03-24 21:34:57 -0500

    added icons

--- src/PollGrader/Setup.js
+++ src/PollGrader/Setup.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Content } from "./PollGrader.styles";
 
 const Setup = ({ handleFile, data }) => {
+  /**
+   * Setup:
+   * titles
+   * instructions
+   * set up popup, select correct answers, ability to re-edit
+   */
+
   return (
     <Content>
       <div>Setup</div>
